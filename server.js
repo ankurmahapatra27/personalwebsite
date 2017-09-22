01e1d38c1070d6b0197d7e4f82743f3ef0218972
ankur <ankurmahapatra27071990@gmail.com>
2017-09-22 17:29:08 +0530

    simple setup

---
+++ server.js
@@ -0,0 +1,27 @@
+/*
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);*/
+
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    path = require('path');
+
+app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}));
+app.use(express.static(path.join(__dirname,'client')));
+
+app.get('/',function(req,res){
+    res.sendfile(__dirname+'/client/index.html');
+    // res.render('index',{title : 'Computer Not Working'});
+})
+
+app.listen(9000);
+
+console.log('server started on 9000');
+module.exports = app;
